d242387d46064d2e66dafc5198b81a0a20b29dbd
SkymeFactor <sergei.51351@yandex.ru>
2022-06-22 14:17:54 +0300

    ADDED: color constants

---
+++ src/main/java/GameColors.java
@@ -0,0 +1,18 @@
+
+
+public abstract class GameColors {
+    public static final int COLOR_BLACK = 0;
+    public static final int COLOR_WHITE = 16777215;
+    public static final int COLOR_LIME = 65280;
+    public static final int COLOR_GREEN = 888832;
+    public static final int COLOR_RED = 16711680;
+    public static final int COLOR_BLUE = 8447;
+    public static final int COLOR_MAGENTA = 16340729;
+    public static final int COLOR_YELLOW = 16777010;
+    public static final int COLOR_PURPLE = 6890345;
+    public static final int COLOR_BEIGE = 12563121;
+    public static final int COLOR_CYAN = 63487;
+
+    public static final int COLOR_ALLY = 11665665;
+    public static final int COLOR_ENEMY = 88518;
+}
